e5238d6b21ff2e50a99334b6435b9b0f9fc28529
Jason Stewart <jaysin.stewart@gmail.com>
2023-04-02 13:02:23 -0700

    Code works but wont pass npm test...

--- index.js
+++ index.js
@@ -11,6 +11,54 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// const titleCased = () => {
+//   return tutorials
+// }
+
+// want to TitleCase the string so => // "What Does The This Keyword Mean?"
+
+// const capitalizeFirstLetter = (word) => {
+//     return word[0].toUpperCase() + word.slice(1)
+// } 
+
+// capitalizeFirstLetter("funkytown")
+
+// const titleCaseString = (str) => {
+//   return str.split(' ').map(function (word) {
+//     return (word[0].toUpperCase() + word.slice(1))
+//   }).join(' ')
+// }
+
+// titleCaseString(testString)
+
+//that works! Now need to iterate through an array of strings executing toTitleCase funciton on each string in the array. Will .map work?
+
+// This works but does not pass the npm test .... no idea why...
+const titleCased = (arrayToTitleCase) => {
+  return arrayToTitleCase.map(function (str) {
+    return str.split(' ').map(function (word) {
+      return (word[0].toUpperCase() + word.slice(1));
+    }).join(' ');
+  })
+  }
+
+console.log(titleCased(tutorials))
+
+
+//This also works but also does not pass the npm test...
+// const titleCaseStrings = (arrayToTitleCase) => {
+//   return arrayToTitleCase.map(function (str) {
+//       return str.split(' ').map(function (word) {
+//           return (word[0].toUpperCase() + word.slice(1))
+//       }).join(' ')
+//       })
+//   }
+
+// const titleCased = (titleCaseStrings, arrayOfStrings) => {
+//   return titleCaseStrings(arrayOfStrings)
+// }
+
+// // Here it is as an arrow funciton... a little unclearer to me in a away but very clean haha
+// const titleCased = (arrayToTitleCase) => {
+//   return arrayToTitleCase.map((str) => str.split(' ').map((word) => (word[0].toUpperCase() + word.slice(1))).join(' '))
+//   }
